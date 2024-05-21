20c9beb485d61177cafa10ecbd6ef187c8252f2d
zzizi6 <130573661+zzizi6@users.noreply.github.com>
2024-05-21 15:40:07 +0900

    image

--- src/js/yolo.jsx
+++ src/js/yolo.jsx
@@ -1,35 +1,39 @@
 import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import '../css/gallery.css';
+
 const user = JSON.parse(sessionStorage.getItem('user'));
 
 
 const Yolo = () => {
   const navigate = useNavigate();
+  let imageList = [];
 
+  useEffect(() => {
 
+    fetchImageUrl(user.userId);
 
-  useEffect(()=>{
-
-
-
-  }, [user]);
+  }, []);
 
   // 이미지 fetch
-  // const fetchImageUrl = async (userId) => {
-  //   const apiUrl = `http://15.164.219.39:8079/find-image/by-kakaoId?kakaoId=${userId}`;
+  const fetchImageUrl = async (userId) => {
+    const apiUrl = `http://15.164.219.39:8079/find-image/by-kakaoId?kakaoId=${userId}`;
 
-  //   try {
-  //     const response = await fetch(apiUrl);
-  //     if (!response.ok) throw new Error('Failed to fetch image');
-  //     const imageUrl = await response.text();
-  //     return `https://greenboogiebucket.s3.ap-northeast-2.amazonaws.com/${imageUrl}`;
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) throw new Error('Failed to fetch image');
+      const imageList = await JSON.parse(response);
 
-  //   } catch (error) {
-  //     console.error('Error fetching image:', error);
-  //     return null;
-  //   }
-  // };
+      // const imageUrl = await response.text();
+
+      // return `https://greenboogiebucket.s3.ap-northeast-2.amazonaws.com/${imageUrl}`;
+
+    } catch (error) {
+      console.error('Error fetching image:', error);
+      return null;
+    }
+  };
 
   return (
     <div className="container">
@@ -42,7 +46,13 @@ const Yolo = () => {
         </div>
       </div>
 
-
+      <div className='gallery'>
+        {imageList.map((src, index) => (
+          <div className='gallery-item' key={index} >
+            <img src={`https://greenboogiebucket.s3.ap-northeast-2.amazonaws.com/${src.imageUUID}`} alt={`Gallery image ${index + 1}`} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
